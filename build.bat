63daa674d76d647ef797a9082c60696d4a9784f7
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2024-09-23 01:43:19 +0300

    Dummy prefs.json for building

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 cd %~dp0
 pyinstaller sortimages_multiview.py --noconfirm
-copy .\libglib-2.0-0.dll .\dist\sortimages_multiview\libglib-2.0-0.dll
-copy .\libgobject-2.0-0.dll .\dist\sortimages_multiview\libgobject-2.0-0.dll
-copy .\libvips-42.dll .\dist\sortimages_multiview\libvips-42.dll
-copy .\libvips-cpp-42.dll .\dist\sortimages_multiview\libvips-cpp-42.dll
+copy .\libglib-2.0-0.dll .\dist\sortimages_multiview\_internal\libglib-2.0-0.dll
+copy .\libgobject-2.0-0.dll .\dist\sortimages_multiview\_internal\libgobject-2.0-0.dll
+copy .\libvips-42.dll .\dist\sortimages_multiview\_internal\libvips-42.dll
+copy .\libvips-cpp-42.dll .\dist\sortimages_multiview\_internal\libvips-cpp-42.dll
+copy .\prefs.json .\dist\sortimages_multiview\prefs.json
 pause
