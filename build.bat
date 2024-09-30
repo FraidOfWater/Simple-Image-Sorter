6f21bbe66627a92a4b422b5d24ead9eac471b0e1
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2024-10-01 02:02:20 +0300

    Update build.bat
    This will check the .spec file for the .dll stuff.

--- build.bat
+++ build.bat
@@ -1,8 +1,3 @@
 cd %~dp0
-pyinstaller sortimages_multiview.py --noconfirm
-copy .\libglib-2.0-0.dll .\dist\sortimages_multiview\_internal\libglib-2.0-0.dll
-copy .\libgobject-2.0-0.dll .\dist\sortimages_multiview\_internal\libgobject-2.0-0.dll
-copy .\libvips-42.dll .\dist\sortimages_multiview\_internal\libvips-42.dll
-copy .\libvips-cpp-42.dll .\dist\sortimages_multiview\_internal\libvips-cpp-42.dll
-copy .\prefs.json .\dist\sortimages_multiview\prefs.json
+pyinstaller sortimages_multiview.spec
 pause
