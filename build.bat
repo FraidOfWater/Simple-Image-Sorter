c4384286651ab014a27ae09ef2aba61904a1dc0c
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2024-11-05 15:34:40 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 cd %~dp0
-pyinstaller --add-data "libvips-cpp-42.dll;." --add-data "libvips-42.dll;." --add-data "libglib-2.0-0.dll;." --add-data "libgobject-2.0-0.dll;." sortimages_multiview.py
+pyinstaller --noconsole --add-data "libvips-cpp-42.dll;." --add-data "libvips-42.dll;." --add-data "libglib-2.0-0.dll;." --add-data "libgobject-2.0-0.dll;." sortimages_multiview.py
+pyinstaller sortimages.py --noconfirm
 pause
