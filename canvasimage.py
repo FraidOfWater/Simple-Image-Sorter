ed177860c9e131e805895b9e11668b4046c44454
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2024-11-05 14:59:31 +0200

    Move autoscrollbar here

--- canvasimage.py
+++ canvasimage.py
@@ -6,10 +6,25 @@ import warnings
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-from autoscrollbar import AutoScrollbar
 import threading
 import time
 import logging
+
+class AutoScrollbar(ttk.Scrollbar):
+	""" A scrollbar that hides itself if it's not needed. Works only for grid geometry manager """
+	def set(self, lo, hi):
+		if float(lo) <= 0.0 and float(hi) >= 1.0:
+			self.grid_remove()
+		else:
+			self.grid()
+			ttk.Scrollbar.set(self, lo, hi)
+
+	def pack(self, **kw):
+		raise tk.TclError('Cannot use pack with the widget ' + self.__class__.__name__)
+
+	def place(self, **kw):
+		raise tk.TclError('Cannot use place with the widget ' + self.__class__.__name__)
+    
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 logging.getLogger("pyvips").setLevel(logging.WARNING)
 class CanvasImage:
