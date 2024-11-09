e33ed8f9174d251abd67e42a6ef1291c6d0ac7b9
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2024-11-09 08:26:50 +0200

    All in one line

--- build.bat
+++ build.bat
@@ -1,7 +1,3 @@
 cd %~dp0
-pyinstaller sortimages_multiview.py --noconfirm
-copy .\libglib-2.0-0.dll .\dist\sortimages_multiview\libglib-2.0-0.dll
-copy .\libgobject-2.0-0.dll .\dist\sortimages_multiview\libgobject-2.0-0.dll
-copy .\libvips-42.dll .\dist\sortimages_multiview\libvips-42.dll
-copy .\libvips-cpp-42.dll .\dist\sortimages_multiview\libvips-cpp-42.dll
+pyinstaller --name "sortimages_multiview.py" --add-data "libvips-cpp-42.dll;." --add-data "libvips-42.dll;." --add-data "libglib-2.0-0.dll;." --add-data "libgobject-2.0-0.dll;." sortimages_multiview.py
 pause
